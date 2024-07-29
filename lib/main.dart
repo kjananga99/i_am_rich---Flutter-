03c306b160902c2d91690cf2ee3784d697708d45
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-29 21:58:44 +0530

    use - Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Jananga'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+            child: Text('Hello Jananga', style: TextStyle(
+              color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        body: Center(child: Image(image: NetworkImage('https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630'),)),
       ),
     ),
   );

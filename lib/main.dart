f9b0bf244a580c9069bf589c928c34503d58de9d
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-30 23:19:25 +0530

    add - AssetImage

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,19 @@ void main() {
         appBar: AppBar(
           backgroundColor: Colors.blueGrey[900],
           title: Center(
-            child: Text('Hello Jananga', style: TextStyle(
-              color: Colors.white,
+            child: Text(
+              'Hello Jananga',
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
           ),
         ),
-        body: Center(child: Image(image: NetworkImage('https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630'),)),
+        body: Center(
+          child: Image(
+            image: AssetImage('android/images/dia.png'),
+          ),
+        ),
       ),
     ),
   );
